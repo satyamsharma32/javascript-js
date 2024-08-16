f4339cc7067f1a4facdfa4c45918fd74228b3894
Satyam Sharma <satyamsharma0012@gmail.com>
2024-08-16 21:12:36 +0000

    container config done

---
+++ 01_basic/variable.js
@@ -0,0 +1,19 @@
+const accountId = 111114
+let accountEmail = "satyamsharma@gmail"
+var accountPassword = "1234"
+accountCity = "mumbai"
+
+// accountId = 2 //  not allowed this in js
+
+// console.log(accountId);
+accountCity = "uk";
+accountPassword = "41123";
+accountEmail= "satyam@sharma";
+let accountState;
+
+console.log([accountId,accountCity,accountPassword,accountEmail,accountState]);
+
+/*
+prefer to not use var variable because of issue of 
+
+*/
