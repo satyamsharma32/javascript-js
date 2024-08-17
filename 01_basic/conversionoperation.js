aa4b5017a775097c785af3c83b3ac53cda0ff05b
Satyam Sharma <satyamsharma0012@gmail.com>
2024-08-17 18:31:09 +0000

    conversion and operation done

---
+++ 01_basic/conversionoperation.js
@@ -0,0 +1,65 @@
+// let score = "33";
+// let score = null;
+// let score = undefined;
+
+// // let marks = 78;
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+// "33" -> 33
+//"33hsd" nan
+// true 1 ; false = 0;
+
+
+// let isLoggedIn = "";
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 true , 0 false 
+ //"" false 
+ // "satyam" true
+
+//  let someNumber = 33
+//  let stringNumber = String(someNumber);
+//  console.log(stringNumber);
+//  console.log(typeof stringNumber);
+
+
+ //`````````````````````````````operation````````````````````````````````
+
+
+
+let value = 3;
+// let negvalue = -value;
+// console.log(negvalue);
+// console.log(value);
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 2); // power
+// console.log(2 / 2);
+// console.log(2 % 3);
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+console.log(true);
+console.log(typeof true);
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
